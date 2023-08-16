ab5f7453d68a4cfe428f8af9b3497de9bed34fcb
Kristiyan Kostadinov <crisbeto@abv.bg>
2023-08-16 10:17:26 +0200

    test(core): remove manually-written control flow instructions
    Updates the control flow tests to use the compiler instead of manually-written instructions. Also adds a couple of tests that I was using along the way to verify that things work as expected.

--- packages/core/test/acceptance/control_flow_exploration_spec.ts
+++ packages/core/test/acceptance/control_flow_exploration_spec.ts
@@ -7,45 +7,19 @@
  */
 
 
-import {ɵɵconditional, ɵɵdefineComponent, ɵɵtemplate, ɵɵtext, ɵɵtextInterpolate} from '@angular/core';
+import {ɵsetEnabledBlockTypes as setEnabledBlockTypes} from '@angular/compiler/src/jit_compiler_facade';
+import {Component, Pipe, PipeTransform} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
 
 describe('control flow', () => {
   describe('if', () => {
-    function App_ng_template_0_Template(rf: number, ctx: unknown) {
-      if (rf & 1) {
-        ɵɵtext(0, 'Something');
-      }
-    }
-    function App_ng_template_1_Template(rf: number, ctx: unknown) {
-      if (rf & 1) {
-        ɵɵtext(0, 'Nothing');
-      }
-    }
+    beforeEach(() => setEnabledBlockTypes(['if']));
+    afterEach(() => setEnabledBlockTypes([]));
 
     it('should add and remove views based on conditions change', () => {
+      @Component({standalone: true, template: '{#if show}Something{:else}Nothing{/if}'})
       class TestComponent {
         show = true;
-        static ɵcmp = ɵɵdefineComponent({
-          type: TestComponent,
-          selectors: [['some-cmp']],
-          decls: 2,
-          vars: 1,
-          template:
-              function TestComponent_Template(rf: number, ctx: any) {
-                if (rf & 1) {
-                  ɵɵtemplate(0, App_ng_template_0_Template, 1, 0);
-                  ɵɵtemplate(1, App_ng_template_1_Template, 1, 0);
-                }
-                if (rf & 2) {
-                  ɵɵconditional(0, ctx.show ? 0 : 1);
-                }
-              },
-          encapsulation: 2
-        });
-        static ɵfac = function TestComponent_Factory(t: any) {
-          return new (t || TestComponent)();
-        };
       }
 
       const fixture = TestBed.createComponent(TestComponent);
@@ -59,73 +33,101 @@ describe('control flow', () => {
     });
 
     it('should expose expression value in context', () => {
-      function App_ng_template_0_Template(rf: number, ctx: any) {
-        if (rf & 1) {
-          ɵɵtext(0, 'Something');
-        }
-        if (rf & 2) {
-          ɵɵtextInterpolate(ctx);
-        }
-      }
-
+      @Component({
+        standalone: true,
+        template: '{#if show; as alias}{{show}} aliased to {{alias}}{/if}',
+      })
       class TestComponent {
         show: any = true;
-        static ɵcmp = ɵɵdefineComponent({
-          type: TestComponent,
-          selectors: [['some-cmp']],
-          decls: 2,
-          vars: 1,
-          template:
-              function TestComponent_Template(rf: number, ctx: any) {
-                if (rf & 1) {
-                  ɵɵtemplate(0, App_ng_template_0_Template, 1, 1);
-                }
-                if (rf & 2) {
-                  let temp: any;
-                  ɵɵconditional(0, (temp = ctx.show) ? 0 : -1, temp);
-                }
-              },
-          encapsulation: 2
-        });
-        static ɵfac = function TestComponent_Factory(t: any) {
-          return new (t || TestComponent)();
-        };
       }
 
       const fixture = TestBed.createComponent(TestComponent);
       fixture.detectChanges();
-      expect(fixture.nativeElement.textContent).toBe('true');
+      expect(fixture.nativeElement.textContent).toBe('true aliased to true');
 
       fixture.componentInstance.show = 1;
       fixture.detectChanges();
-      expect(fixture.nativeElement.textContent).toBe('1');
+      expect(fixture.nativeElement.textContent).toBe('1 aliased to 1');
     });
 
+    it('should expose the context to nested conditional blocks', () => {
+      @Component({
+        standalone: true,
+        template: `
+          {#if show === 2}
+            {:else if show; as alias}
+              {#if show > 0}
+                {#if show !== 7}{{alias}}{/if}
+              {/if}
+          {/if}
+        `,
+      })
+      class TestComponent {
+        show = 1;
+      }
+
+      const fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toBe('1');
+
+      fixture.componentInstance.show = 3;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toBe('3');
+    });
+
+    it('should expose expression value in context', () => {
+      @Component({
+        standalone: true,
+        template: '{#if show; as alias}{{show}} aliased to {{alias}}{/if}',
+      })
+      class TestComponent {
+        show: any = true;
+      }
+
+      const fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toBe('true aliased to true');
+
+      fixture.componentInstance.show = 1;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toBe('1 aliased to 1');
+    });
+
+    it('should expose expression value passed through a pipe in context', () => {
+      @Pipe({name: 'multiply', pure: true, standalone: true})
+      class MultiplyPipe implements PipeTransform {
+        transform(value: number, amount: number) {
+          return value * amount;
+        }
+      }
+
+      @Component({
+        standalone: true,
+        template: '{#if value | multiply:2; as alias}{{value}} aliased to {{alias}}{/if}',
+        imports: [MultiplyPipe],
+      })
+      class TestComponent {
+        value = 1;
+      }
+
+      const fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toBe('1 aliased to 2');
+
+      fixture.componentInstance.value = 4;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.textContent).toBe('4 aliased to 8');
+    });
+
+    // QUESTION: fundamental mismatch between the "template" and "container" concepts
+    // those 2 calls to the ɵɵtemplate instruction will generate comment nodes and LContainer
     it('should destroy all views if there is nothing to display', () => {
+      @Component({
+        standalone: true,
+        template: '{#if show}Something{/if}',
+      })
       class TestComponent {
         show = true;
-        static ɵcmp = ɵɵdefineComponent({
-          type: TestComponent,
-          selectors: [['some-cmp']],
-          decls: 1,
-          vars: 1,
-          template:
-              function TestComponent_Template(rf: number, ctx: any) {
-                if (rf & 1) {
-                  // QUESTION: fundamental mismatch between the "template" and "container" concepts
-                  // those 2 calls to the ɵɵtemplate instruction will generate comment nodes and
-                  // LContainer
-                  ɵɵtemplate(0, App_ng_template_0_Template, 1, 0);
-                }
-                if (rf & 2) {
-                  ɵɵconditional(0, ctx.show ? 0 : -1);
-                }
-              },
-          encapsulation: 2
-        });
-        static ɵfac = function TestComponent_Factory(t: any) {
-          return new (t || TestComponent)();
-        };
       }
 
       const fixture = TestBed.createComponent(TestComponent);
@@ -139,66 +141,39 @@ describe('control flow', () => {
   });
 
   describe('switch', () => {
-    function App_ng_template_0_Template(rf: number, ctx: unknown) {
-      if (rf & 1) {
-        ɵɵtext(0, 'case 0');
-      }
-    }
+    beforeEach(() => setEnabledBlockTypes(['switch']));
+    afterEach(() => setEnabledBlockTypes([]));
 
-    function App_ng_template_1_Template(rf: number, ctx: unknown) {
-      if (rf & 1) {
-        ɵɵtext(0, 'case 1');
-      }
-    }
-
-    function App_ng_template_default_Template(rf: number, ctx: unknown) {
-      if (rf & 1) {
-        ɵɵtext(0, 'default');
-      }
-    }
-
+    // Open question: == vs. === for comparison
+    // == is the current Angular implementation
+    // === is used by JavaScript semantics
     it('should show a template based on a matching case', () => {
+      @Component({
+        standalone: true,
+        template: `
+          {#switch case}
+            {:case 0}case 0
+            {:case 1}case 1
+            {:default}default
+          {/switch}
+        `
+      })
       class TestComponent {
         case = 0;
-        static ɵcmp = ɵɵdefineComponent({
-          type: TestComponent,
-          selectors: [['some-cmp']],
-          decls: 3,
-          vars: 1,
-          template:
-              function TestComponent_Template(rf: number, ctx: any) {
-                if (rf & 1) {
-                  ɵɵtemplate(0, App_ng_template_0_Template, 1, 0);
-                  ɵɵtemplate(1, App_ng_template_1_Template, 1, 0);
-                  ɵɵtemplate(2, App_ng_template_default_Template, 1, 0);
-                }
-                if (rf & 2) {
-                  const expValue = ctx.case;
-                  // Open question: == vs. === for comparison
-                  // == is the current Angular implementation
-                  // === is used by JavaScript semantics
-                  ɵɵconditional(0, expValue === 0 ? 0 : expValue === 1 ? 1 : 2);
-                }
-              },
-          encapsulation: 2
-        });
-        static ɵfac = function TestComponent_Factory(t: any) {
-          return new (t || TestComponent)();
-        };
       }
 
       const fixture = TestBed.createComponent(TestComponent);
       fixture.detectChanges();
 
-      expect(fixture.nativeElement.textContent).toBe('case 0');
+      expect(fixture.nativeElement.textContent).toBe('case 0 ');
 
       fixture.componentInstance.case = 1;
       fixture.detectChanges();
-      expect(fixture.nativeElement.textContent).toBe('case 1');
+      expect(fixture.nativeElement.textContent).toBe('case 1 ');
 
       fixture.componentInstance.case = 5;
       fixture.detectChanges();
-      expect(fixture.nativeElement.textContent).toBe('default');
+      expect(fixture.nativeElement.textContent).toBe('default ');
     });
   });
 });
